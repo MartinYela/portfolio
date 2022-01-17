1d6f6d6b3d35a315b72e8cc52c003b2610e0c906
MartinYela <martinyela@hotmail.com>
2022-01-17 14:14:07 +0100

    mejoras

--- portfolio/src/components/cardProject/cardProject.jsx
+++ portfolio/src/components/cardProject/cardProject.jsx
@@ -1,9 +1,21 @@
 import './style.css';
 
-export default function CardProject(props){
-    return(
-        <div className='card_container'>
-            <img className='imgProject' src={props.imagen} alt="imagen del proyecto" />
+export default function CardProject(props) {
+    const style = {
+        'background-image': `url(${props.imagen})`,
+        'background-size': 'cover'
+    }
+    return (
+        <div className='cardBox'>
+            <div className='card'>
+                <div className='front' style={style}>
+                    {/* <img className='imgProject' src={} alt="imagen del proyecto" /> */}
+                </div>
+                <div className='back'>
+                    <p className='text'>{props.texto}</p>
+                    <a className='enlace' href={props.web}>Ir al proyecto</a>
+                </div>
+            </div>
         </div>
     )
 }
--- portfolio/src/components/projects/projects.jsx
+++ portfolio/src/components/projects/projects.jsx
@@ -1,18 +1,33 @@
 import CardProject from "../cardProject/cardProject";
 import './style.css';
-import projectTiempo from '../../imagenes/app-tiempo.png';
+import projectTiempo from '../../imagenes/tiempoOk.png';
 import projectHTML from '../../imagenes/Landing.png';
-import projectTetris from '../../imagenes/tetris.png'
+import projectTetris from '../../imagenes/tetris2.png'
 import { Fragment } from "react";
+
+const textoProyectos = [
+    {
+        "appTiempo" : "Proyecto realizado en conjunto con alumnos de IX UI. Aprendimos a utilizar paletas de componentes (material ui) y solidificamos conocimientos de React y llamadas a API. Es una versión móvil",
+        "enlace" : "https://pedroac98.github.io/tiempo-app/"
+    },
+    {
+        "html-css" : "Primer proyecto del bootcamp, realizado utilizando solamente HTML5 y CSS3",
+        "enlace" : "https://martinyela.github.io/proyecto-html-css/index.html"
+    },
+    {
+        "tetris" : "Proyecto muy divertido del famoso juego del tetris. Práctica realizada con Javascript, y la vista con HTML5 y CSS3",
+        "enlace" : "https://martinyela.github.io/tetris/"
+    }
+]
 
 export default function Projects() {
     return (
         <Fragment>
             <h1 className="title" id='proyectos'>Proyectos</h1>
             <div className="project_container">
-                <CardProject imagen={projectTiempo}></CardProject>
-                <CardProject imagen={projectHTML}></CardProject>
-                <CardProject imagen={projectTetris}></CardProject>
+                <CardProject imagen={projectTiempo} texto={textoProyectos[0].appTiempo} web={textoProyectos[0].enlace}></CardProject>
+                <CardProject imagen={projectHTML} texto={textoProyectos[1]["html-css"]} web={textoProyectos[1].enlace}></CardProject>
+                <CardProject imagen={projectTetris} texto={textoProyectos[2].tetris}  web={textoProyectos[2].enlace}></CardProject>
             </div>
         </Fragment>
     )
