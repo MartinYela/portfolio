dc15963246fc03df88a0538a6f061e9abb109a2e
MartinYela <martinyela@hotmail.com>
2022-01-18 12:58:35 +0100

    menu hamburguesa

--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -8,11 +8,20 @@ export default function Header() {
                 <a href="https://www.linkedin.com/in/martín-alfredo-yela"><img className='linkedin' src={logoLinkedin} alt="logo linkedin" /></a>
                 <p>692580956</p>
             </div>
-            <div>
+            <div className='menu_grande'>
                 <a href='#me'>SOBRE MI</a>
                 <a href='#formacion'>FORMACION</a>
                 <a href='#proyectos'>PROYECTOS</a>
             </div>
+            <nav className='boton'>
+                <input type="checkbox" id="menu"/>
+                    <label for="menu"> ☰ </label>
+                    <ul>
+                        <li>SOBRE MI</li>
+                        <li>FORMACION</li>
+                        <li>PROYECTOS</li>
+                    </ul>
+            </nav>
 
         </header>
     )
