9ecde96aadfec7a9fe46a35363e7e300b6a4d0a4
MartinYela <martinyela@hotmail.com>
2022-01-18 19:23:44 +0100

    gusta

--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function Footer() {
     return (
         <footer className='datos_contacto'>
-            <p>Si te gusto mi perfil, no dudes en contactarme!!!</p>
+            <p>Si te gusta mi perfil, no dudes en contactarme!!!</p>
             <p>martinalfredoyela@gmail.com</p>
             <p>692580956</p>
         </footer>
